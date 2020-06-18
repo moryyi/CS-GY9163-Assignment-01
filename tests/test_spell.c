eb1b453eb9815ef432b64b221d1635b23659bdb2
Danyang Song <ds6118@nyu.edu>
2020-06-18 16:40:39 -0400

    Update .gitignore. Change some file locations.

--- tests/test_spell.c
+++ tests/test_spell.c
@@ -3,6 +3,9 @@
 
 #include "../spell-check/dictionary.h"
 
+#define TEST_DICTIONARY_FILENAME "./test_dictionary.txt"
+#define COMPLETE_DICTIONARY_FILENAME "./wordlist.txt"
+#define MAX_MISSPELLED 2000
 
 /** 
  * Test Case 01.
@@ -10,11 +13,10 @@
  */
 START_TEST(test_load_normal_dictionary) {
   hashmap_t hashtable[HASH_SIZE];
-  char* test_dictionary_filename = "./test_dictionary.txt";
   bool if_loaded;
   hashmap_t tmp, head;
 
-  if_loaded = load_dictionary(test_dictionary_filename, hashtable);
+  if_loaded = load_dictionary(TEST_DICTIONARY_FILENAME, hashtable);
   
   // 1. Check return value of load_dictionary()
   ck_assert_int_eq(true, if_loaded);
@@ -43,6 +45,57 @@ START_TEST(test_load_normal_dictionary) {
   }
 } END_TEST
 
+/**
+ * Test Case 02.
+ * Test given words are correctly checked by check_word()
+ * 
+ */
+START_TEST (test_check_word_normal) {
+  hashmap_t hashtable[HASH_SIZE];
+  hashmap_t head, tmp;
+  load_dictionary(COMPLETE_DICTIONARY_FILENAME, hashtable);
+    // Words to be checked
+  // 1. word containing only letters
+  const char* correct_word_01 = "earthling";
+  // 2. word containing letters and (')
+  const char* correct_word_02 = "earthling's";
+  // 3. word containing only digits
+  const char* correct_word_03 = "12345";
+  // 4. word containing mixed letters and digits
+  const char* wrong_word_01 = "earthling123";
+  // 5. word not in the dictionary
+  const char* wrong_word_02 = "definitenotindictionaryword";
+
+  // check_word() should return true
+  ck_assert(check_word(correct_word_01, hashtable));
+  ck_assert(check_word(correct_word_02, hashtable));
+  ck_assert(check_word(correct_word_03, hashtable));
+
+  // check_word() should return false
+  ck_assert(!check_word(wrong_word_01, hashtable));
+  ck_assert(!check_word(wrong_word_02, hashtable));
+
+  // Free allocated memory to avoid memory leak.
+  for (int i = 0; i < HASH_SIZE; i++) {
+    head = hashtable[i];
+    while (head != NULL) {
+      tmp = head;
+      head = head->next;
+      free(tmp);
+    }
+    hashtable[i] = NULL;
+  }
+} END_TEST
+
+
+/**
+ * Test Case 0.
+ * Normal text file: contains only english words (with misspelled words).
+ */
+START_TEST (test_only_english_words) {
+
+} END_TEST
+
 
 /**
  * Test Suite
@@ -52,9 +105,11 @@ Suite* test_spell_suite(void) {
   TCase* tc_core;
 
   s = suite_create("test_spell");
-  tc_core = rcase_create("core");
-  
+  tc_core = tcase_create("core");
+
+  // Add test case here
   tcase_add_test(tc_core, test_load_normal_dictionary);
+  tcase_add_test(tc_core, test_check_word_normal);
 
   suite_add_tcase(s, tc_core);
 
